d3e837f42a7c19ef6f76ca45a469cea7c122cd3c
OpenTech-Consult <opentech.consult@gmail.com>
2022-12-02 16:45:42 +0000

    List and Keys
    created a state and initialized it with an array of items
    
    created a ul element and inside it map through the items list and for each element of the map, rendered a li element that contains a check box and a label of the item.

--- src/Content.js
+++ src/Content.js
@@ -3,37 +3,38 @@ import { useState } from 'react'
 
 const Content = () => {
 
-    const [name, setName] = useState('Sandro')
-    const [count, setCount] = useState(0)
+    const [items, setItems] = useState([
+        {
+            id: 1,
+            checked: false,
+            item: "One half pound bag of Cocoa covered Almonds Unsalted"
+        },
+        {
+            id: 2,
+            checked: false,
+            item: "Item 2"
+        },
+        {
+            id: 3,
+            checked: false,
+            item: "Item 3"
+        }
+    ])
 
-    const handleNameChange = () => {
-        const names = ['Bob', 'Kevin', 'John', 'Dave', 'Sandro']
-        const int = Math.floor(Math.random() * 5)
-        setName(names[int])
-    }
-
-    const handleClick = () => {
-        setCount(count + 1)
-        setCount(count + 1)
-        console.log(count)
-    }
-
-    const handleClick2 = () => {
-        console.log(count)
-    }
-
-    const handleClick3 = (e) => {
-        console.log(e.target.innerText)
-    }
 
     return (
         <main>
-            <p onDoubleClick={handleClick}>
-                Hello {name}!
-            </p>
-            <button onClick={handleNameChange}>Change name</button>
-            <button onClick={handleClick}>Click it!</button>
-            <button onClick={handleClick2}>Click it!</button>
+            <ul>
+                {items.map((item) => (
+                    <li className="item" key={item.id}>
+                        <input
+                            type="checkbox"
+                            checked={item.checked} />
+                        <label>{item.item}</label>
+                        <button>Delete</button>
+                    </li>
+                ))}
+            </ul>
         </main>
     )
 }

d65ddbaef4e65575a16af642d11bb60da682cc55
OpenTech-Consult <opentech.consult@gmail.com>
2022-12-02 16:24:20 +0000

    useStateHook
    learn how to define state and how modify state in React

--- src/Content.js
+++ src/Content.js
@@ -1,18 +1,25 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Content = () => {
+
+    const [name, setName] = useState('Sandro')
+    const [count, setCount] = useState(0)
+
     const handleNameChange = () => {
         const names = ['Bob', 'Kevin', 'John', 'Dave', 'Sandro']
         const int = Math.floor(Math.random() * 5)
-        return names[int]
+        setName(names[int])
     }
 
     const handleClick = () => {
-        console.log('You clicked it')
+        setCount(count + 1)
+        setCount(count + 1)
+        console.log(count)
     }
 
-    const handleClick2 = (name) => {
-        console.log(`${name} was clicked`)
+    const handleClick2 = () => {
+        console.log(count)
     }
 
     const handleClick3 = (e) => {
@@ -22,11 +29,11 @@ const Content = () => {
     return (
         <main>
             <p onDoubleClick={handleClick}>
-                Hello {handleNameChange()}!
+                Hello {name}!
             </p>
+            <button onClick={handleNameChange}>Change name</button>
             <button onClick={handleClick}>Click it!</button>
-            <button onClick={() => handleClick2('Sandro')}>Click it!</button>
-            <button onClick={(e) => handleClick3(e)}>Click it!</button>
+            <button onClick={handleClick2}>Click it!</button>
         </main>
     )
 }

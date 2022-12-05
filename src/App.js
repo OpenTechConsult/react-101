3a5f831d4eae9e4c7be1394d4c8f5b38d6c61242
OpenTech-Consult <opentech.consult@gmail.com>
2022-12-05 11:43:16 +0000

    Props and Prop drilling
    change the footer component by adding the length property. recup the property in the footer component code and use it to display the number of items in the list.

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ function App() {
         items={items}
         handleCheck={handleCheck}
         handleDelete={handleDelete} />
-      <Footer />
+      <Footer length={items.length} />
     </div>
   )
 }
